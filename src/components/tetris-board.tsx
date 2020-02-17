99806542acdfc6e7d30027b5f0f9591243d76607
khagayn <kh.guy@Hotmail.com>
2020-02-17 01:13:09 -0500

    Add tetris-board.tsx props

---
+++ src/components/tetris-board.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+// Define props for TetrisBoard component
+type TetrisBoardProps = {
+    fields: any[],
+    gameOver: boolean,
+    score: number,
+    level: number,
+    rotate: number
+}
+
+//
